8a6c6809cbefaa5b4d4cf80bf272dd77ab500992
Maxim <36982547+Siphoin@users.noreply.github.com>
2025-03-05 15:46:30 +0500

    Initial commit

---
+++ source.w3x/war3map.lua
@@ -0,0 +1,79 @@
+gg_trg_Melee_Initialization = nil
+function InitGlobals()
+end
+
+function Trig_Melee_Initialization_Actions()
+MeleeStartingVisibility()
+MeleeStartingHeroLimit()
+MeleeGrantHeroItems()
+MeleeStartingResources()
+MeleeClearExcessUnits()
+MeleeStartingUnits()
+MeleeStartingAI()
+MeleeInitVictoryDefeat()
+end
+
+function InitTrig_Melee_Initialization()
+gg_trg_Melee_Initialization = CreateTrigger()
+TriggerAddAction(gg_trg_Melee_Initialization, Trig_Melee_Initialization_Actions)
+end
+
+function InitCustomTriggers()
+InitTrig_Melee_Initialization()
+end
+
+function RunInitializationTriggers()
+ConditionalTriggerExecute(gg_trg_Melee_Initialization)
+end
+
+function InitCustomPlayerSlots()
+SetPlayerStartLocation(Player(0), 0)
+SetPlayerColor(Player(0), ConvertPlayerColor(0))
+SetPlayerRacePreference(Player(0), RACE_PREF_HUMAN)
+SetPlayerRaceSelectable(Player(0), true)
+SetPlayerController(Player(0), MAP_CONTROL_USER)
+SetPlayerStartLocation(Player(11), 1)
+SetPlayerColor(Player(11), ConvertPlayerColor(11))
+SetPlayerRacePreference(Player(11), RACE_PREF_NIGHTELF)
+SetPlayerRaceSelectable(Player(11), true)
+SetPlayerController(Player(11), MAP_CONTROL_COMPUTER)
+end
+
+function InitCustomTeams()
+SetPlayerTeam(Player(0), 0)
+SetPlayerTeam(Player(11), 0)
+end
+
+function InitAllyPriorities()
+SetEnemyStartLocPrioCount(1, 2)
+SetEnemyStartLocPrio(1, 0, 0, MAP_LOC_PRIO_HIGH)
+end
+
+function main()
+SetCameraBounds(-13568.0 + GetCameraMargin(CAMERA_MARGIN_LEFT), -13824.0 + GetCameraMargin(CAMERA_MARGIN_BOTTOM), 13568.0 - GetCameraMargin(CAMERA_MARGIN_RIGHT), 13312.0 - GetCameraMargin(CAMERA_MARGIN_TOP), -13568.0 + GetCameraMargin(CAMERA_MARGIN_LEFT), 13312.0 - GetCameraMargin(CAMERA_MARGIN_TOP), 13568.0 - GetCameraMargin(CAMERA_MARGIN_RIGHT), -13824.0 + GetCameraMargin(CAMERA_MARGIN_BOTTOM))
+SetDayNightModels("Environment\\DNC\\DNCLordaeron\\DNCLordaeronTerrain\\DNCLordaeronTerrain.mdl", "Environment\\DNC\\DNCLordaeron\\DNCLordaeronUnit\\DNCLordaeronUnit.mdl")
+NewSoundEnvironment("Default")
+SetAmbientDaySound("LordaeronSummerDay")
+SetAmbientNightSound("LordaeronSummerNight")
+SetMapMusic("Music", true, 0)
+InitBlizzard()
+InitGlobals()
+InitCustomTriggers()
+RunInitializationTriggers()
+end
+
+function config()
+SetMapName("TRIGSTR_001")
+SetMapDescription("TRIGSTR_003")
+SetPlayers(2)
+SetTeams(2)
+SetGamePlacement(MAP_PLACEMENT_USE_MAP_SETTINGS)
+DefineStartLocation(0, 9024.0, -9216.0)
+DefineStartLocation(1, -9872.1, 4738.4)
+InitCustomPlayerSlots()
+SetPlayerSlotAvailable(Player(0), MAP_CONTROL_USER)
+SetPlayerSlotAvailable(Player(11), MAP_CONTROL_COMPUTER)
+InitGenericPlayerSlots()
+InitAllyPriorities()
+end
+
