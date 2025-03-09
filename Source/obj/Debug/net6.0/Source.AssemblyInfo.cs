3895a8e8247bcf47408a047f2eba6245ab902a3d
Maxim <36982547+Siphoin@users.noreply.github.com>
2025-03-09 18:39:34 +0500

    fixs

--- Source/obj/Debug/net6.0/Source.AssemblyInfo.cs
+++ Source/obj/Debug/net6.0/Source.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Source")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6681f0d0cacbb4ca4c67f0ee477f7222040e9865")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23bb361dfbffadf4a7f4c02215c65b42eba926fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Source")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Source")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
