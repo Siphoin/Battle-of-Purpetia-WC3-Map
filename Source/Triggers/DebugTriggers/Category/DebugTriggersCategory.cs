f6458e7d6b93af464c86d9859d167fbe0b9b2273
Maxim <36982547+Siphoin@users.noreply.github.com>
2025-03-20 19:54:25 +0500

    Update DebugTriggersCategory.cs

--- Source/Triggers/DebugTriggers/Category/DebugTriggersCategory.cs
+++ Source/Triggers/DebugTriggers/Category/DebugTriggersCategory.cs
@@ -1,6 +1,7 @@
 ﻿using Source.Triggers.Base;
 using Source.Triggers.DebugTriggers.Triggers;
 using System.Collections.Generic;
+using WCSharp.Api;
 
 namespace Source.Triggers.DebugTriggers.Category
 {
@@ -25,19 +26,29 @@ namespace Source.Triggers.DebugTriggers.Category
             PauseUnitDebugTrigger pauseUnitDebugTrigger = new PauseUnitDebugTrigger();
             ArenaTimerOnDebugTrigger arenaTimerOnDebugTrigger = new ArenaTimerOnDebugTrigger();
             ArenaTimerOffDebugTrigger arenaTimerOffDebugTrigger = new ArenaTimerOffDebugTrigger();
-            triggers.Add(dungeonDebugExecuterTrigger);
-            triggers.Add(setHeroLevelDebugTrigger);
-            triggers.Add(killAllCreepsDebugTrigger);
-            triggers.Add(killUnitDebugTrigger);
-            triggers.Add(setCurrentManaDebugTrigger);
-            triggers.Add(setCurrentHPDebugTrigger);
-            triggers.Add(setMaxManaDebugTrigger);
-            triggers.Add(setMaxHPDebugTrigger);
-            triggers.Add(createUnitAtXYDebugTrigger);
-            triggers.Add(setUnitXYDebugTrigger);
-            triggers.Add(pauseUnitDebugTrigger);
-            triggers.Add(arenaTimerOnDebugTrigger);
-            triggers.Add(arenaTimerOffDebugTrigger);
+
+
+            TriggerInstance[] debugTriggers = new TriggerInstance[]
+            {
+                dungeonDebugExecuterTrigger,
+                setHeroLevelDebugTrigger,
+                killAllCreepsDebugTrigger,
+                killUnitDebugTrigger,
+                killAllCreepsDebugTrigger,
+                setCurrentHPDebugTrigger,
+                setMaxHPDebugTrigger,
+                setMaxManaDebugTrigger,
+                setCurrentManaDebugTrigger,
+                setUnitXYDebugTrigger,
+                createUnitAtXYDebugTrigger,
+                arenaTimerOffDebugTrigger,
+                arenaTimerOnDebugTrigger,
+                pauseUnitDebugTrigger,
+            };
+            foreach (var triggerInstance in debugTriggers)
+            {
+                triggers.Add(triggerInstance);
+            }
 #endif
             return triggers;
         }
