7db66d4879b32d1a1b311606a5c6a979831f62f5
Maxim <36982547+Siphoin@users.noreply.github.com>
2025-03-21 17:12:38 +0500

    Update QuestSystem.cs

--- Source/Systems/QuestSystem.cs
+++ Source/Systems/QuestSystem.cs
@@ -1,21 +1,44 @@
 ﻿using Source.Data.Quests;
 using System.Collections.Generic;
 using System.Linq;
+using WCSharp.Api;
 namespace Source.Systems
 {
     public static class QuestSystem
     {
-        private static List<QuestInstance> _quests = new();
+        private static Dictionary<player, List<QuestInstance>> _quests = new();
 
-        public static IEnumerable<QuestInstance> Quests => _quests;
+        public static IEnumerable<QuestInstance> GetQuestsByPlayer (player player)
+        {
+            if (!_quests.TryGetValue(player, out var instance))
+            {
+                return Enumerable.Empty<QuestInstance>();
+            }
+
+            return instance;
+        }
 
         public static void RegisterQuest (QuestInstance quest)
         {        
-            if (!_quests.Any(x => x.GetTitle() == quest.GetTitle()))
+            if (!_quests.Any(x => x.Value.Contains(quest)))
             {
                 quest.Init();
                 quest.GetTrigger();
-                _quests.Add(quest);
+
+                if (_quests.TryGetValue(quest.PlayerOwner, out var quests))
+                {
+                    quests.Add(quest);
+                }
+
+                else
+                {
+                    List<QuestInstance> newQuestList = new List<QuestInstance>()
+                    {
+                        quest,
+                    };
+
+                    _quests.Add(quest.PlayerOwner, quests);
+                }
             }
         }
     }
